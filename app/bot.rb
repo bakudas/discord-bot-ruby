9c0ef162f085ab3d9a364e65b6f2ceea2465caff
bakudas <bakudas@gmail.com>
2022-12-16 03:36:45 -0300

    some encapsulating things

--- app/bot.rb
+++ app/bot.rb
@@ -2,216 +2,23 @@
 
 require 'discordrb'
 require 'dotenv/load'
-require "uri"
-require "net/http"
-require 'discordrb/webhooks'
-require './app/utils/get_data'
+require './app/utils/load_commands'
 
 Dotenv.require_keys('DISCORD_TOKEN') # required keys and tokens
 
-bot = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], prefix: '!', intents: [:server_messages]) # initializing the bot
+bot = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], name: 'Purple Bit', prefix: '!', intents: [:server_messages]) # initializing the bot
+
 #bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'], intents: [:server_messages]) # initializing the bot
 
-hora = Time.now.ctime
+date = Time.now.ctime
 
-puts '-------------------------------------'
-puts "Logado como: #{bot.name} às #{hora}"
-puts '-------------------------------------'
+LoadCommands.new bot
+
+puts '-----------------------------------------'
+puts "Logado como: #{bot.name} às #{date}"
+puts '-----------------------------------------'
 puts 'Comandos carregados:'
 
-
-bot.command :d do |event, side|
-  result = rand(1..side.to_i).round
-  event << "#{event.user.name}, você tirou no dado :game_die:: #{result}"
-end
-
-bot.command :bakratos do |event|
-  bot.send_file(event.channel, File.open('app/data/images/Bakratos.png', 'r'))
-end
-
-bot.command(:alongamento, description: 'Uma breve seleção de alongamentos para evitar lesões ao passar muito tempo na frente do pc.') do |event|
-  bot.send_file(event.channel, File.open('app/data/images/alongamento.png', 'r'))
-end
-
-bot.command :aquaman do |event|
-  bot.send_file(event.channel, File.open('app/data/images/aquaman.png', 'r'))
-end
-
-bot.command :artenilson do |event|
-  bot.send_file(event.channel, File.open('app/data/images/artenilson.png', 'r'))
-end
-
-bot.command :peitao do |event|
-  bot.send_file(event.channel, File.open('app/data/images/babyelias.gif', 'r'))
-end
-
-bot.command :bongiorno do |event|
-  bot.send_file(event.channel, File.open('app/data/images/bongiornogif.gif', 'r'))
-end
-
-bot.command :bundifa do |event|
-  bot.send_file(event.channel, File.open('app/data/images/bundifa.gif', 'r'))
-end
-
-bot.command :muçei do |event|
-  bot.send_file(event.channel, File.open('app/data/images/buximchei.png', 'r'))
-end
-
-bot.command :concordo do |event|
-  bot.send_file(event.channel, File.open('app/data/images/concordo.gif', 'r'))
-end
-
-bot.command :discordo do |event|
-  bot.send_file(event.channel, File.open('app/data/images/discordo.gif', 'r'))
-end
-
-bot.command :effe do |event|
-  bot.send_file(event.channel, File.open('app/data/images/EfeVacinado.jpg', 'r'))
-end
-
-bot.command :excuse do |event|
-  bot.send_file(event.channel, File.open('app/data/images/excuse.gif', 'r'))
-end
-
-bot.command :fumito do |event|
-  bot.send_file(event.channel, File.open('app/data/images/fumito.png', 'r'))
-end
-
-bot.command :funko do |event|
-  bot.send_file(event.channel, File.open('app/data/images/funko_vaca.png', 'r'))
-end
-
-bot.command :kek do |event|
-  bot.send_file(event.channel, File.open('app/data/images/kek.gif', 'r'))
-end
-
-bot.command :lulz do |event|
-  bot.send_file(event.channel, File.open('app/data/images/lulz.png', 'r'))
-end
-
-bot.command :meudeus do |event|
-  bot.send_file(event.channel, File.open('app/data/images/meudeus.png', 'r'))
-end
-
-bot.command :pixelart101 do |event|
-  bot.send_file(event.channel, File.open('app/data/images/pixelart101.png', 'r'))
-end
-
-bot.command :raffamoreiraman do |event|
-  bot.send_file(event.channel, File.open('app/data/images/raffamoreiraman.gif', 'r'))
-end
-
-bot.command :regras do |event|
-  bot.send_file(event.channel, File.open("app/data/images/regras0#{rand(1...3)}.jpg", 'r'))
-end
-
-bot.command :show do |event|
-  bot.send_file(event.channel, File.open("app/data/images/show.gif", 'r'))
-end
-
-bot.command :top do |event|
-  bot.send_file(event.channel, File.open("app/data/images/top.gif", 'r'))
-end
-
-bot.command :topkek do |event|
-  bot.send_file(event.channel, File.open("app/data/images/topkek.png", 'r'))
-end
-
-bot.command :tucurte do |event|
-  bot.send_file(event.channel, File.open("app/data/images/tucurte.jpg", 'r'))
-end
-
-bot.command :gato do |_event|
-  cat_api = "https://api.thecatapi.com/v1/images/search?mime_types=gif"
-
-  cat = GetData.new cat_api
-
-  cat.response[0]["url"]
-end
-
-bot.command :pokemon do |event, pkm|
-  if pkm.nil?
-    event << "#{event.user.name}, como vou trazer informações do pokemão se não me disse qual?"
-    event << 'Tente, por exemplo: `!pokemon pikachu`.'
-    return
-  end
-
-  pkm_url = "https://pokeapi.co/api/v2/pokemon/#{pkm}" # get data from pokeapi v2 -> pokemon name or number
-
-  data = GetData.new(pkm_url) # GetData object with following instance variables -> :url, :data
-
-  if data.status.eql?"404"
-    event << '<:berro_do_trovao:394286932756004865>'
-    event << "#{event.user.name}, `#{pkm}` não parece ser um pokemão válido segundo o prof Oak ¬¬"
-    event << 'Tem certeza que esse é mesmo o nome de pokemão?'
-    return
-  end
-
-  # WEBHOOK -> #OFFTOPIC
-  client = Discordrb::Webhooks::Client.new(url: ENV['WEBHOOK_URL'])
-
-  # WEBHOOK -> #SERVIDOR_TESTE
-  # client = Discordrb::Webhooks::Client.new(url: ENV['WEBHOOK_URL_TESTE'])
-  
-  # EMBED
-  client.execute do |builder|
-    builder.content = 'Quem é esse pokemon?'
-    builder.add_embed do |embed|
-      embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: data.response["sprites"]["front_default"])
-      embed.title = data.response["name"].capitalize
-      embed.add_field(name: 'Type', value: "1: #{data.response["types"][0]["type"]["name"]}
-                                            2: #{data.response["types"][1] != nil ? data.response["types"][1]["type"]["name"] : "-" }", inline: true)
-      embed.add_field(name: 'Initial Moves', value: "1: #{data.response["moves"][0] != nil ? data.response["moves"][0]["move"]["name"] : "-" }
-                                            2: #{data.response["moves"][1] != nil ? data.response["moves"][1]["move"]["name"] : "-" }
-                                            3: #{data.response["moves"][2] != nil ? data.response["moves"][2]["move"]["name"] : "-" }
-                                            4: #{data.response["moves"][3] != nil ? data.response["moves"][3]["move"]["name"] : "-" }", inline: true)
-
-      embed.description = " "
-      embed.image = Discordrb::Webhooks::EmbedImage.new(url: data.response["sprites"]["other"]["official-artwork"]["front_default"])
-      embed.timestamp = Time.now
-    end
-  end
-
-end
-
-bot.command :policia do |event|
-  event << "<:vacacop:417882011378909184>  <:vacacop:417882011378909184>"
-  event << ":rotating_light: :rotating_light:"
-  event << "#{event.user.name}, declara que para fins de investigação policial,"
-  event << "não ter envolvimento com este grupo e não saber como"
-  event << "está no mesmo, provavelmente foi inserido por terceiros,"
-  event << "declara também que está disposto e apto a colaborar com "
-  event << "as investigações e se apresentar para depoimento "
-  event << "caso seja necessário."
-  event << "<:vacacop:417882011378909184>  <:vacacop:417882011378909184>"
-  event << ":rotating_light: :rotating_light:"
-end
-
-# AUDIOS
-bot.command :bomdia do |event|
-  bot.send_file(event.channel, File.open('app/data/audio/bomdiaSebby.mp3', 'r'))
-end
-
-bot.command :essabarra do |event|
-  bot.send_file(event.channel, File.open('app/data/audio/essabarra.mp3', 'r'))
-end
-
-bot.command :superhot do |event|
-  bot.send_file(event.channel, File.open('app/data/audio/superhot.mp3', 'r'))
-end
-
-bot.command(:eval, help_available: false) do |event, *code|
-  break unless event.user.id == 194927916268781569 # Replace number with your ID
-
-  begin
-    eval code.join(' ')
-  rescue StandardError
-    'An error occurred 😞'
-  end
-end
-
-bot.commands.each { |symbol, _command, _command_alias| puts symbol }
-
-puts bot.name
+bot.commands.sort.each { |symbol, _command, _command_alias| puts symbol }
 
 bot.run
--- app/commands/teste.rb
+++ app/commands/teste.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 module Teste
-  attr_reader :commands
-
   include Discordrb::Commands::CommandContainer
-
-  def initialize
-    @commands = teste
-  end
+  
+  attr_reader :commands
 
 end
