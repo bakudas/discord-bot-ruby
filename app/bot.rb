70de8f1b1de3fe1f4374bbe9b2cff70df972670b
wilson melo <bakudas@gmail.com>
2023-07-17 17:46:05 -0300

    some code refactor

--- app/bot.rb
+++ app/bot.rb
@@ -1,24 +1,32 @@
-# bot.rb
+# fronzen_string_literal: true
+# Discord bot by vacaroxa
 
 require 'discordrb'
 require 'dotenv/load'
 require './app/utils/load_commands.rb'
 
+# LOAD ENV
 Dotenv.require_keys('DISCORD_TOKEN') # required keys and tokens
 
+# CREATE BOT
 bot = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], name: 'Purple Bit', prefix: '!', intents: [:server_messages]) # initializing the bot
 
 #bot = Discordrb::Bot.new(token: ENV['DISCORD_TOKEN'], intents: [:server_messages]) # initializing the bot
 
+# GET TIME
 date = Time.now.ctime
 
+puts '-----------------------------------------'
+puts "- #{bot.name} - #{date} -"
+
+# LOAD ALL COMMANDS
 LoadCommands.new bot
 
 puts '-----------------------------------------'
-puts "Logado como: #{bot.name} às #{date}"
-puts '-----------------------------------------'
-puts 'Comandos carregados:'
+puts '-------- Comandos carregados ------------'
 
-bot.commands.sort.each { |symbol, _command, _command_alias| puts symbol }
+# Print loaded commands
+bot.commands.sort.each { |symbol, _command, _command_alias| print "!" + symbol.to_s + ", " }
 
+# BOT RUN
 bot.run
--- app/utils/load_commands.rb
+++ app/utils/load_commands.rb
@@ -1,31 +1,13 @@
-require 'dotenv/load'
-require "uri"
-require "net/http"
-require 'discordrb/webhooks'
-require './app/utils/get_data'
-
 Dir.glob("./app/commands/*.rb").reject { |file| require file }
 
 class LoadCommands
   include Discordrb::Commands::CommandContainer
 
   def initialize bot
-
     cmd_list = []
 
     Dir.glob("./app/commands/*.rb").reject { |file| cmd_list << file.split("/").last.split(".").first.capitalize }
 
     cmd_list.each { |command| bot.include!(Module.const_get command) }
-
-    # AUDIOS
-    bot.command(:eval, help_available: false) do |event, *code|
-      break unless event.user.id == 194927916268781569 # Replace number with your ID
-      
-      begin
-        eval code.join(' ')
-      rescue StandardError
-        'An error occurred 😞'
-      end
-    end
   end
 end
